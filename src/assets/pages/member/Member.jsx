337febd98b57cc08820ee717c3daf447b989d66d
Yen <yuxxxlouyen@gmail.com>
2025-03-05 18:09:05 +0800

    分評價

--- src/assets/pages/member/Member.jsx
+++ src/assets/pages/member/Member.jsx
@@ -6,7 +6,7 @@ import { useDispatch } from 'react-redux';
 import { createAsyncMessage } from "../../redux/slice/toastSlice";
 import IsScreenLoading from "../../component/IsScreenLoading";
 import FormatDate from "../../component/formatDate";
-import { Modal } from 'bootstrap';
+import CommentModal from "../../component/CommentModal";
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
 const apiPath = import.meta.env.VITE_API_PATH;
@@ -21,7 +21,6 @@ function Member() {
     const [expandedOrders, setExpandedOrders] = useState(new Set());
     const [textExtend, setTextExtend] = useState(false);
     const modelRef = useRef(null);
-    const orderlRef = useRef(null);
     const [tempProduct, setTempProduct] = useState({})
 
 
@@ -87,23 +86,13 @@ function Member() {
 
     //modal
     const openModal = (product) => {
-        console.log(product)
         setTempProduct(product);
         modelRef.current.show()
     }
-
-    useEffect(() => {
-        modelRef.current = new Modal(orderlRef.current, {
-            backdrop: false
-        })
-    }, [])
-
-    const closeModal = () => {
-        modelRef.current.hide()
-    }
+    
     return (<>
         <div className="container member">
-            <div className="row my-7 g-5">
+            <div className="row my-lg-7 my-0 g-5">
                 <div className="col-lg-3">
                     <div className="card border-0 d-none d-lg-block">
                         <div className="card-body bg-secondary-200 rounded rounded-3 mb-3 px-5 pb-5 pt-11 position-relative w-100" style={{ marginTop: "132px" }}>
@@ -168,7 +157,7 @@ function Member() {
                         </div>
                     </div>
                 </div>
-                <div className="col-lg-9 p-5">
+                <div className="col-lg-9 p-lg-5 p-0">
                     {/* <!-- pc版 --> */}
                     <main className="mt-6 d-none d-lg-block">
                         <h2 className="fs-4 text-primary">我的訂單</h2>
@@ -289,64 +278,7 @@ function Member() {
                 })
             }
         </main >
-        {/* <!-- Modal --> */}
-        < div className="modal" tabIndex="-1" ref={orderlRef} style={{ backgroundColor: "rgba(0,0,0,0.5)" }}>
-            <div className="modal-dialog modal-md">
-                <div className="modal-content">
-                    <div className="modal-header py-6 px-5 border-0">
-                        <h5 className="modal-title">填寫評價</h5>
-                        <button type="button" className="btn-close me-1" onClick={closeModal} aria-label="Close"></button>
-                    </div>
-                    <div className="modal-body px-5 mb-6">
-                        <div className="d-flex p-4" style={{ backgroundColor: "rgba(250, 250, 250, 1)" }}>
-                            <img src={tempProduct.product?.imageUrl} alt={tempProduct.id} className=" object-fit-cover rounded-1 me-5" style={{ width: "90px", height: "90px" }} />
-                            <div className="my-auto">
-                                <p className="fs-5 fw-bold">{tempProduct.product?.title}</p>
-                                <p className="fs-7 text-muted">{tempProduct.product?.description}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div
-                        className="modal-body d-flex flex-column align-items-stretch text-start px-5">
-                        <h6 className="mb-2 text-dark fs-5">請為此商品評分：</h6>
-                        <div className="mb-5">
-                            <span
-                                className="material-symbols-outlined text-accent fs-5">star</span>
-                            <span
-                                className="material-symbols-outlined text-accent fs-5">star</span>
-                            <span
-                                className="material-symbols-outlined text-accent fs-5">star</span>
-                            <span
-                                className="material-symbols-outlined text-accent fs-5">star</span>
-                            <span
-                                className="material-symbols-outlined text-accent fs-5">star</span>
-                        </div>
-                        <div className="mb-5">
-                            <label htmlFor="commentTitle" className="form-label">姓名</label>
-                            <input type="text" className="form-control mb-3" id="commentTitle"
-                                placeholder="請輸入姓名 (將會顯示在商品評論)" />
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked />
-                                <label class="form-check-label" for="flexCheckDefault">
-                                    記住我的姓名，下次評論使用
-                                </label>
-                            </div>
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="commentContent" className="form-label">內文</label>
-                            <textarea className="form-control" id="commentContent" rows="3"
-                                placeholder="快來分享你的購物體驗！(最多 100 字)"></textarea>
-                        </div>
-                    </div>
-                    <div className="modal-footer border-0 justify-content-center py-0 mb-5">
-                        <button className="btn btn-primary fw-bold py-3 w-100 mb-3 text-white"
-                            data-bs-target="#m-commitModel2"
-                            data-bs-toggle="modal">送出評價</button>
-                        <small>請注意，送出後無法變更評價</small>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <CommentModal tempProduct={tempProduct} modelRef={modelRef}/>
         <Toast />
         <img src="src/assets/images/Illustration/Top-Curve.png" alt="banner" className="promotion-curve" />
         <IsScreenLoading isScreenLoading={isScreenLoading} />
