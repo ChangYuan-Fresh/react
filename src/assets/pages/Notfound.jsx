1d647d60945461074863504d7a9195476ec60764
yilingliu <liu61024@gmail.com>
2025-03-18 23:59:45 +0800

    NotFound微調

--- src/assets/pages/Notfound.jsx
+++ src/assets/pages/Notfound.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 function Notfound (){
     return (<>
         <div className="container px-5  py-5 d-flex flex-column rounded-4 justify-content-center align-items-center">
-            <i class="text-center text-primary bi bi-exclamation-triangle" style={{fontSize:"100px"}}></i>
-            <h1 className="text-center">找不到網頁!</h1>
-            <Link className="card-link link-primary pt-3" to="/">回首頁</Link> 
+            <img src="images/18470.png" alt="?" style={{width:"100px"}} />
+            <h1 className="text-center p-3">找不到網頁</h1>
+            <Link className="card-link link-primary" to="/">回首頁</Link> 
         </div> 
     </>
     )
--- src/assets/pages/admin/AdminHomePage.jsx
+++ src/assets/pages/admin/AdminHomePage.jsx
@@ -1,8 +1,8 @@
 function AdminHomePage() {
     return (<>
         <div className="container px-5  py-5 d-flex flex-column rounded-4 justify-content-center align-items-center">
-            <h1 className="text-center">歡迎進入管理中心!</h1>
-            <i class="text-center text-primary bi bi-house-check-fill" style={{fontSize:"100px"}}></i>
+            <h1 className="text-center text-decoration-color">歡迎進入管理中心!</h1>
+            <img src="images/21247.png" className="py-3" alt="?" style={{width:"120px"}} />
         </div>
     </>
     )
